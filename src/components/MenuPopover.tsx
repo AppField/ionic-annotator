a58156867c03134fdf5553ae6e81516c0d420963
Roman Mühlfeldner <roman@muehlfeldner.com>
2020-04-11 15:54:43 +0200

    Added delete and download functionality to menu popover

--- src/components/MenuPopover.tsx
+++ src/components/MenuPopover.tsx
@@ -1,19 +1,60 @@
-import React from "react";
-import { IonList, IonItem, IonLabel, IonIcon } from "@ionic/react";
+import React, { useState } from "react";
+import { IonList, IonItem, IonLabel, IonIcon, IonAlert } from "@ionic/react";
 import { downloadOutline, trashOutline } from "ionicons/icons";
+import { useAnnotateContext } from "../pages/AnnotatorManager";
+import { jsonToCSV } from "react-papaparse";
 
-const MenuPopover: React.FC = () => (
-  <IonList>
-    <IonItem button>
-      <IonIcon slot="start" icon={trashOutline} />
-      <IonLabel>Herunterladen</IonLabel>
-    </IonItem>
-    <IonItem button>
-      <IonIcon slot="start" icon={downloadOutline} />
-      <IonLabel>Daten L&ouml;schen</IonLabel>
-    </IonItem>
-  </IonList>
-);
+const MenuPopover: React.FC = () => {
+  const { annotate, setAnnotate } = useAnnotateContext();
+  const [showAlert, setShowAlert] = useState(false);
+
+  return (
+    <>
+      <IonAlert
+        isOpen={showAlert}
+        onDidDismiss={() => setShowAlert(false)}
+        header="Daten wirklich löschen?"
+        message="Die Daten werden unwiderruflich gelöscht."
+        buttons={[
+          {
+            text: "Löschen",
+            handler: () => {
+              setAnnotate([]);
+            },
+          },
+          {
+            text: "Abbrechen",
+            role: "cancel",
+          },
+        ]}
+      />
+
+      <IonList>
+        <IonItem
+          button
+          onClick={() => {
+            const csv = jsonToCSV(annotate);
+            const csvBlob = new Blob([csv], {
+              type: "text/csv;charset=utf-8;",
+            });
+            const csvUrl = window.URL.createObjectURL(csvBlob);
+            const tempLink = document.createElement("a");
+            tempLink.href = csvUrl;
+            tempLink.setAttribute("download", "sentiments.csv");
+            tempLink.click();
+          }}
+        >
+          <IonIcon slot="start" icon={trashOutline} />
+          <IonLabel>Herunterladen</IonLabel>
+        </IonItem>
+        <IonItem button onClick={() => setShowAlert(true)}>
+          <IonIcon slot="start" icon={downloadOutline} />
+          <IonLabel>Daten L&ouml;schen</IonLabel>
+        </IonItem>
+      </IonList>
+    </>
+  );
+};
 
 export default MenuPopover;
 
